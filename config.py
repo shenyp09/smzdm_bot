7c3c1427c7b600362b03a8b034e53808d96404b3
ArcXya <shenyp09@gmail.com>
2022-05-16 23:23:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = '__jsluid_h=e678ccd60b1604c518694ad33db989e7; __ckguid=c9k4YKxTb3C5AAa7yIvFKW; device_id=21307064331652712631220831e4a58d13de87d51e5eee5c36875c9f7e; homepage_sug=d; r_sort_type=score; __jsluid_s=df2a3c3653c164de5b7268df6146141b; _zdmA.vid=*; sajssdk_2015_cross_new_user=1; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1652712634; footer_floating_layer=0; ad_date=16; ad_json_feed=%7B%7D; smzdm_user_view=E8C55743663E4C5E8814437DD5AA7C03; smzdm_user_source=CC23B0F875F0897690C2BFC83C45D41E; sess=AT-mP044GA%2FN5rUOkEHCpYFdkpJLyLIasYEmiiyC963bLD%2BmT%2FvwNUd0TpVv2mD0%2FcV88bW3k%2B8BPXG1MqZs1h8tsNmxmouccYwUBralu4lu4wPZjPOYmSS1gOE; user=sina_tezf8%7C8788108687; smzdm_id=8788108687; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22180cd59720b14-0a27fce9d8f98b-357d6702-1296000-180cd59720c402%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Fwww.smzdm.com%2F%22%7D%2C%22%24device_id%22%3A%22180cd59720b14-0a27fce9d8f98b-357d6702-1296000-180cd59720c402%22%7D; _zdmA.uid=ZDMA.a6vkKFTwR.1652712819.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1652712820; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; _zdmA.time=1652712820503.0.https%3A%2F%2Fwww.smzdm.com%2F'
-
+TEST_COOKIE = ''
 
 """
 调试用 SERVERCHAN_SECRETKEY
